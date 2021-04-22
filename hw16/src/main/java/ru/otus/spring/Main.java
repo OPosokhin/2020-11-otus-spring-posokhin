10d34f8d012db1df8a5d496b29eaf98c0d1fa25d
oposokhin <53135044+OPosokhin@users.noreply.github.com>
2021-04-22 18:48:39 +0300

    hw16. Использовать метрики, healthchecks и logfile (#14)
    * hw14 2020-11 spring. Spring Batch миграции данных из реляционного хранилища в NoSQL.
    
    * hw16. Использовать метрики, healthchecks и logfile

---
+++ hw16/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication
+@EnableWebMvc
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ hw16/src/main/java/ru/otus/spring/rest/BookController.java
@@ -0,0 +1,64 @@
+package ru.otus.spring.rest;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.service.AuthorService;
+import ru.otus.spring.service.BookService;
+import ru.otus.spring.service.GenreService;
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+    private final GenreService genreService;
+    private final AuthorService authorService;
+
+
+    public BookController(BookService bookService, GenreService genreService, AuthorService authorService) {
+        this.bookService = bookService;
+        this.genreService = genreService;
+        this.authorService = authorService;
+
+    }
+
+
+    @GetMapping("/book")
+    public String bookPage(Model model) {
+        model.addAttribute("book", bookService.getAll());
+        return "book";
+    }
+
+
+    @PostMapping("/bookAdd")
+    public String addBook(Model model) {
+        Book book = new Book();
+        book.setId(0l);
+        model.addAttribute("book", book);
+        model.addAttribute("author", authorService.getAll());
+        model.addAttribute("genre", genreService.getAll());
+        return "bookAdd";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String getBook(@PathVariable("id") Long id, Model model) {
+        Book book = bookService.getById(id);
+        model.addAttribute("author", authorService.getAll());
+        model.addAttribute("genre", genreService.getAll());
+        model.addAttribute("book", book);
+        return "bookEdit";
+    }
+
+
+    @PostMapping("/book/{bookId}/delete")
+    public String delete(@PathVariable long bookId) {
+        bookService.delete(bookId);
+        return "redirect:/book";
+    }
+
+}
+
